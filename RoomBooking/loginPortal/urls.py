e7643c506c9e8ba5859b2223af6484ed53ad3898
trisha <trisha@teredesai.com>
2025-03-29 17:16:13 -0700

    fixing errors

--- RoomBooking/loginPortal/urls.py
+++ RoomBooking/loginPortal/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from .views import authView, home
+
 
 urlpatterns=[
     path("signup/", authView, name="authView"),
