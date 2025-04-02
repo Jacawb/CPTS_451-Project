9eeb09f32504cb1fcbfd4c08231ac2ff6fbd9f84
zhengfel <felix.zheng@wsu.edu>
2025-04-02 00:30:33 -0700

    Fix dorm selection dropdown

--- RoomBooking/RoomBookingWebsite/views.py
+++ RoomBooking/RoomBookingWebsite/views.py
@@ -8,6 +8,12 @@ def home(request):
 
 def room_browsing(request):
     buildings = Building.objects.all()
-    available_rooms = Room.objects.filter(is_available=True)
+    selected_building_id = request.GET.get('building_id', '')
+
+    # Filter rooms based on selected building
+    if selected_building_id:
+        available_rooms = Room.objects.filter(building_id=selected_building_id, is_available=True)
+    else:
+        available_rooms = Room.objects.filter(is_available=True)
 
     return render(request, 'RoomBookingWebsite/RoomBrowsing.html', {'buildings': buildings, 'rooms': available_rooms})
