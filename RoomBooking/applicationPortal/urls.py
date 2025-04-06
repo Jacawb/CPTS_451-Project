d6215146488fb2ae36b8791f88627dbaa2f9a30c
trisha <trisha@teredesai.com>
2025-04-06 00:56:22 -0700

    updating application HTML templates

--- RoomBooking/applicationPortal/urls.py
+++ RoomBooking/applicationPortal/urls.py
@@ -5,5 +5,6 @@ urlpatterns=[
     path('', views.start, name="application-start"),
     path('page1/', views.pg1, name="application-pg1"),
     path('page2/', views.pg2, name="application-pg2"),
+    path('page3/', views.pg3, name="application-pg3"),
     path('confirm/', views.confirmation, name="application-confirmation"),
 ]
